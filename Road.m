9536236eb2350a745863c41bfa745a9571a93eab
Karam Safarov <karam.safarov@uwe.ac.uk>
2018-05-03 22:49:23 +0100

    proper density prescription

--- Road.m
+++ Road.m
@@ -4,7 +4,7 @@ classdef Road < handle
         startPoint = 0
         endPoint = 0
         Width = 0
-        BtCarsRatio = 0
+        allCarsNumArray = 0
         priority
         orientation = ''
         crossingBeginPosition = NaN
@@ -18,33 +18,35 @@ classdef Road < handle
         nCarHistory = 0
         averageVelocityHistory = []
         carType = 0
+        carTypes = {}
     end
     methods
         function obj = Road(road_args)
             if nargin == 0
                 error('input format should be Road([x1 x2],[y1 y2],theta)')
             else
-                obj.startPoint = road_args(1);
-                obj.endPoint = road_args(2);
-                obj.Width = road_args(3);
+                obj.startPoint = road_args{1};
+                obj.endPoint = road_args{2};
+                obj.Width = road_args{3};
                 obj.crossingBeginPosition = -obj.Width/2;
                 obj.crossingEndPosition = obj.Width/2;
-                theta = road_args(4);
-                obj.BtCarsRatio = road_args(6);
+                theta = road_args{4};
+                obj.carTypes = road_args{6};
+                obj.allCarsNumArray = road_args{7};
                 if theta == 0
                     obj.orientation = 'horizontal';
-                    if road_args(5) == 1
+                    if road_args{5} == 1
                         obj.priority = true;
-                    elseif road_args(5) == 0
+                    elseif road_args{5} == 0
                         obj.priority = true;
                     else
                         obj.priority = NaN;
                     end
                 else
                     obj.orientation = 'vertical';
-                    if road_args(5) == 1
+                    if road_args{5} == 1
                         obj.priority = false;
-                    elseif road_args(5) == 0
+                    elseif road_args{5} == 0
                         obj.priority = false;
                     else
                         obj.priority = NaN;
@@ -53,12 +55,22 @@ classdef Road < handle
             end
         end
         function add_car(obj,prescription)
-            if obj.carType == 1
-                new_car = BtCar(obj.orientation, prescription, obj.startPoint, obj.Width);
-            else
-                new_car = IdmCar(obj.orientation, prescription, obj.startPoint, obj.Width);
+            allCars = [];
+            for i = 1:numel(obj.allCarsNumArray)
+                if obj.allCarsNumArray(i) > 0
+                    for j = 1:obj.allCarsNumArray(i)
+                        new_car = obj.carTypes{i}(obj.orientation, prescription, obj.startPoint, obj.Width);
+                        allCars = [allCars new_car];
+                    end
+                end
             end
-            obj.allCars = [obj.allCars new_car];
+          obj.allCars = allCars(randperm(length(allCars)));
+%             if obj.carType == 1
+%                 new_car = BtCar(obj.orientation, prescription, obj.startPoint, obj.Width);
+%             else
+%                 new_car = IdmCar(obj.orientation, prescription, obj.startPoint, obj.Width);
+%             end
+%             obj.allCars = [obj.allCars new_car];
         end
         function move_all_cars(obj,dt)
             for iCar = 1:obj.numCars
--- prescribe_density.m
+++ prescribe_density.m
@@ -2,35 +2,59 @@ close all
 clear
 
 prescription = 'density';
-carTypes = {@Car, @Idm, @BtCar};
-HorizArmBtCarRatio = 0.5; % per cent of cars are BtCars
-VertArmBtCarRatio = 1; % per cent of cars are BtCars
-carTypeRatios= [HorizArmBtCarRatio VertArmBtCarRatio];
+carTypes = {@Car, @IdmCar, @BtCar};
+carTypeRatios = [0 0.5 0.5;
+                 0 0 1];
+             
 plotFlag = false;
 runTime = 3600; % in seconds
 timeStep = 0.1; % in seconds
 priority = true;
 roadDimensions = [-200 100 4];
-numberOfSimRuns = 8;
-occupancy = {logspace(log10(0.5),log10(0.25),numberOfSimRuns);
-    logspace(log10(0.5),log10(0.25),numberOfSimRuns)};
+InitNumberOfSimRuns = 25;
+noSpawnAreaLength = 20; % length of no spawn area around the junction 
 
-% density = {0.166-logspace(log10(0.166),log10(0.001),numberOfSimRuns);
-%     logspace(0.166-log10(0.166),log10(0.001),numberOfSimRuns)};
+init_density.horizontal = 0.05-logspace(log10(0.001),log10(0.035),InitNumberOfSimRuns);
+init_density.vertical = 0.05-logspace(log10(0.001),log10(0.035),InitNumberOfSimRuns);
+
+[numCars.horizontal(:), idx]= unique(round(init_density.horizontal(:) * (roadDimensions(2) - roadDimensions(1) - noSpawnAreaLength)),'first');
+[numCars.vertical(:), idx1] = unique(round(init_density.vertical(:) * (roadDimensions(2) - roadDimensions(1) - noSpawnAreaLength)),'first');
+
+numCars.horizontal = flip(numCars.horizontal);
+numCars.vertical = flip(numCars.vertical);
+% numCars.vertical = numCars.vertical*ones(1,numel(numCars.horizontal));
+
+numberOfSimRuns = numel(numCars.horizontal);
+density.horizontal = numCars.horizontal/(roadDimensions(2) - roadDimensions(1));
+density.vertical = numCars.vertical/(roadDimensions(2) - roadDimensions(1));
 
 for k = 1:numberOfSimRuns
-    [sim(k)] = driverscript_density(...
-        carTypeRatios,...
+    for i = 1:numel(carTypes)
+        if i == numel(carTypes)
+            allCarsNumArray(k).horizontal(i) = numCars.horizontal(k) - sum(allCarsNumArray(k).horizontal(1:i-1));
+            allCarsNumArray(k).vertical(i) = numCars.vertical(k) - sum(allCarsNumArray(k).vertical(1:i-1));
+        else
+            allCarsNumArray(k).horizontal(i) = round(numCars.horizontal(k)*carTypeRatios(1,i));
+            allCarsNumArray(k).vertical(i) = round(numCars.vertical(k)*carTypeRatios(2,i));
+        end
+    end
+end
+
+for k = 1:numberOfSimRuns
+    sim(k) = driverscript_density(...
+        carTypes,...
+        [allCarsNumArray(k).horizontal; allCarsNumArray(k).vertical],...
         runTime,...
         plotFlag,...
         priority,...
         roadDimensions,...
-        [occupancy{1}(k); occupancy{2}(k)],...
+        [density.horizontal(k); density.vertical(k)],...
+        [numCars.horizontal(k); numCars.vertical(k)],...
         timeStep);
 end
 
-save('/Users/robot/car_sim_mat_Files/density_29_04.mat','prescription','carTypeRatios','runTime','timeStep',...
-    'plotFlag','occupancy','priority','roadDimensions','nIterations','k','sim','-v7.3')
+save('/Users/robot/car_sim_mat_Files/density_02_05_1_1.mat','prescription','carTypeRatios','carTypes','allCarsNumArray',...
+    'runTime','timeStep','plotFlag','density','priority','roadDimensions','k','sim','-v7.3')
 
 % save real_data
 % p = profile('info');
