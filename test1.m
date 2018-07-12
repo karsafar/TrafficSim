98352a11df659be84408fe3af393011ae097fa0e
Karam <karam.safarov24@gmail.com>
2018-07-12 23:33:58 +0100

    random density prescription is done, need to add some rules for random flow prescription as well to start initially with certain number of cars at the start.

--- test1.m
+++ test1.m
@@ -20,7 +20,7 @@ roadDims.Start = [-150; -150];
 roadDims.End = [150; 150];
 roadDims.Width = [4; 4];
 roadDims.Length = roadDims.End - roadDims.Start;
-selectRoadTypes = [1 2] ;
+selectRoadTypes = [1 1] ;
 
 nIterations = runTime/dt;
 nDigits = numel(num2str(dt))-2;
@@ -32,19 +32,25 @@ distMeanRange = [7, 10; 7, 10];
 %% Decide type of road parameters
 [subRoadArgs,numberOfSimRuns] = prescribe_traffic(selectRoadTypes,numberOfSimRuns,carTypes,carTypeRatios,fixedSeed,roadDims,densityRange,distMeanRange);
 
-
-sz = [2 4];
-varTypes = {'double','double','double','function_handle'};
-varNames = {'position','velocity','acceleration','carType'};
-% table of all cars inputed manually
-T = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
-T.position = [20;-20];
-T.velocity = [5;7];
-T.acceleration = [1;1];
-T.carType = {carTypes{3};carTypes{3}};
-H_Arm = SpawnCars(T,'horizontal',roadDims.Start(1),roadDims.Width(1),dt);
-V_arm = SpawnCars(T,'vertical',roadDims.Start(1),roadDims.Width(1),dt);
-
+manualFlag = 0;
+if manualFlag
+    %% needs a UI with dialogbox to input paramenters
+    sz = [2 4];
+    varTypes = {'double','double','double','function_handle'};
+    varNames = {'position','velocity','acceleration','carType'};
+    % table of all cars inputed manually
+    T = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
+    T.position = [20;-20];
+    T.velocity = [5;7];
+    T.acceleration = [1;1];
+    T.carType = {carTypes{3};carTypes{3}};
+    H_Arm = SpawnCars(T,'horizontal',roadDims.Start(1),roadDims.End(1),roadDims.Width(1),dt);
+    V_arm = SpawnCars(T,'vertical',roadDims.Start(2),roadDims.End(2),roadDims.Width(2),dt);
+else
+    % need a loop to go through 1 to k subroad arguments
+    H_Arm = SpawnCars([subRoadArgs(1).Horizontal,{carTypes}],'horizontal',roadDims.Start(1),roadDims.End(1),roadDims.Width(1),dt);
+    V_arm = SpawnCars([subRoadArgs(1).Vertical,{carTypes}],'vertical',roadDims.Start(2),roadDims.End(2),roadDims.Width(1),dt);
+end
 %% run simulations
 for k = 1:numberOfSimRuns
     sim(k) = run_simulation({roadTypes{selectRoadTypes(1)},roadTypes{selectRoadTypes(2)}},carTypes,subRoadArgs(k),t_rng,plotFlag,priority,roadDims,nIterations,dt);
