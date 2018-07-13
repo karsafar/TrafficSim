19f90a11ce8021af94b8f0fe53b14666e80113cb
Karam <karam.safarov24@gmail.com>
2018-07-13 21:58:51 +0100

    input box for manually inputed cars is created. Need to do sanity checks on them.

--- decide_input.m
+++ decide_input.m
@@ -1,15 +1,27 @@
 function [Arm] = decide_input(manualFlag,carTypes,roadDims,subRoadArgs,dt)
+
 for i = 1:2
     if manualFlag(i)
-        sz = [2 4];
+        if i == 1
+            msg = ('Horizontal arm input');
+        else
+            msg = ('Vertical arm input');
+        end
+        title = msg;
+        prompt = {'Number of vehicles','Positions','Velocites','Accelerations',' Control types'};
+        dims = [1 50; 1 50; 1 50; 1 50; 1 50];
+        definput = {'2', '20 -20', '5 7', '1 1','3 3'};
+        answer = inputdlg(prompt,title,dims,definput);
+        
+        sz = [str2num(answer{1}) 4];
         varTypes = {'double','double','double','function_handle'};
         varNames = {'position','velocity','acceleration','carType'};
-
+        
         T = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
-        T.position = [20;-20];
-        T.velocity = [5;7];
-        T.acceleration = [1;1];
-        T.carType = {carTypes{3};carTypes{3}};
+        T.position = [str2num(answer{2})'];
+        T.velocity = [str2num(answer{3})'];
+        T.acceleration = [str2num(answer{4})'];
+        T.carType = {carTypes{str2num(answer{5})'}}';
         
         if i == 1
             Arm.H = SpawnCars(T,'horizontal',roadDims.Start(1),roadDims.End(1),roadDims.Width(1),dt);
