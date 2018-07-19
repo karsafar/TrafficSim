2317b695682cf4b343818c7795d51b95cd0e99c7
Karam <karam.safarov24@gmail.com>
2018-07-19 14:45:38 +0100

    Test single arm running

--- test1.m
+++ test1.m
@@ -5,8 +5,8 @@ clear
 roadTypes = {@LoopRoad @FiniteRoad};
 carTypes = {@IdmCar, @HdmCar, @AggressiveCar, @PassiveCar, @HesitantCar, @ManualCar};
 
-% carTypeRatios = [0 0 1 0 0 0; 0 0 1 0 0 0];
-carTypeRatios = [0 0 0 0 0 1;0 0 0.25 0.15 0.15 0.45];
+carTypeRatios = [0 0 1 0 0 0; 0 0 1 0 0 0];
+% carTypeRatios = [0 0 1 0 0 0;0 0 0.25 0.15 0.15 0.45];
 assert(sum(carTypeRatios(1,:)) == 1,'Wrong distribution of horizontal arm rations');
 assert(sum(carTypeRatios(2,:)) == 1,'Wrong distribution of vertical arm rations');
 
@@ -14,10 +14,10 @@ plotFlag = true;
 runTime = 3600; % in seconds
 dt = 0.1; % in seconds
 priority = true;
-fixedSeed = [true true];
+fixedSeed = [false false];
 % road dimensions
-roadDims.Start = [-150; -150];
-roadDims.End = [150; 150];
+roadDims.Start = [-500; -150];
+roadDims.End = [500; 150];
 roadDims.Width = [4; 4];
 roadDims.Length = roadDims.End - roadDims.Start;
 selectRoadTypes = [1 1] ;
@@ -26,7 +26,7 @@ nIterations = runTime/dt;
 nDigits = numel(num2str(dt))-2;
 t_rng = round(linspace(0,runTime,nIterations),nDigits);
 numberOfSimRuns = 20;
-densityRange = [0.04, 0.04; 0.04, 0.04];
+densityRange = [0.01, 0.15; 0.0001, 0.0001];
 distMeanRange = [7, 10; 7, 10];
 
 %% Decide type of road parameters
